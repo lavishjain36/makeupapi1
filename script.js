0aa88a6f4cd4a39db851f91170e02a4106b56d61
Lavish <lavishjain36@gmail.com>
2021-11-24 20:27:48 +0530

    first commi3

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
          
 async function getAlldata() {
   try {
-    var data = await fetch("https://makeup-api.herokuapp.com/api/v1/products.json");
+    var data = await fetch("http://makeup-api.herokuapp.com/api/v1/products.json");
     var obj = await data.json();
     console.log(obj);
 
